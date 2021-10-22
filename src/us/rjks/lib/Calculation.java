11dda177a775e398e9045b9b674cabb8b0a133dc
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-10-22 19:05:22 +0200

    removed debug

--- src/us/rjks/lib/Calculation.java
+++ src/us/rjks/lib/Calculation.java
@@ -36,32 +36,19 @@ public class Calculation {
 
         switch(binaryTree.getContent().getOperand()) {
             case '*': {
-                System.out.println(5);
-
                 return l * r;
             }
             case '/': {
-                System.out.println(6);
-
                 if (r != 0.0) return l / r;
                 else return Double.NaN;
             }
             case '+': {
-                System.out.println(7);
-
-
                 return l + r;
             }
             case '-': {
-
-                System.out.println(8);
-
                 return l - r;
             }
             default: {
-                System.out.println(9);
-
-
                 return Double.NaN;
             }
         }
